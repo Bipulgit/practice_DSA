2b71febf6fde6cae7ccfe4cc20ffaa5622e0b34d
Bipul <bipul.kumar3384@gmail.com>
2023-05-29 19:40:31 +0530

    package211.js

---
+++ w3resource 12.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,n;
+	cout<<"Input 1st number: "<<endl;
+	cin>>a;
+	cout<<"Input 2nd number: "<<endl;
+	cin>>b;
+	n = a+b;
+	cout<<"Sum of the number is: "<<n<<endl;
+	return 0;
+}
