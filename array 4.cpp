cff884097816fc4ae5d2969f78ea1e5e7c526cdb
Bipul <bipul.kumar3384@gmail.com>
2023-06-29 22:31:43 +0530

    added array4.cpp

---
+++ array 4.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+using namespace std;
+
+   //using main function
+   
+int main()
+{
+	int a[10];
+	
+	// using loop
+	for(int i=0; i<10; i++)
+	{
+		cout<<"Enter a number: "<<endl;
+		cin>>a[i];
+	}
+	return 0;
+	
+	                   //NOT SOLVED
+	                   
+	/*Take 10 integer inputs from user and store them in an array. 
+	Again ask user to give a number.
+	 Now, tell user whether that number is present in array or not.*/                   
+}
