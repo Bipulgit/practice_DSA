2f83dc40dc3ee7cd3576940f39cc423b03a36202
Bipul <bipul.kumar3384@gmail.com>
2023-07-07 22:40:32 +0530

    cando2.cpp

---
+++ class and object 2.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+using namespace std;
+
+//using class
+
+class student
+{
+	public:
+		
+	    string name1;
+	    int roll1,phn_no1;
+	    string add1;
+	    
+	    
+};
+
+// main function
+int main()
+{
+	student s;
+	s.name1 = "Sam","john";
+	s.roll1 = 1,2;
+	s.phn_no1 = 2323413,2342351;
+	s.add1 = "ara bihar","aehduowehfewf";
+	
+	return 0;
+}
