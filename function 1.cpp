d340d2bb7f005caa81120a92072519afa67c44f7
Bipul <bipul.kumar3384@gmail.com>
2023-07-31 23:27:20 +0530

    createbinarytree5.cpp

---
+++ function 1.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+
+//using function
+
+int sum(int x,int y)
+{
+	return x+y;
+}
+
+//using main function
+
+int main()
+{
+	cout<<sum(2,4)<<endl;
+	return 0;
+}
