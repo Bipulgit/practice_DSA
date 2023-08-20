ac604334e954155f919f13d507294b18bad51fe9
Bipul <bipul.kumar3384@gmail.com>
2023-08-20 18:01:57 +0530

    insertionsort10.cpp

---
+++ jfjhfjh154847979520.5488989560345290908768428zfdgldtweuhw[][]]}{}bvuiwgesdbkbv,kk.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+     int i;
+     cin>>i;
+
+     while (i!=8)
+     {
+         cout<<"access denied"<<endl;
+        cin>>i;
+     }
+     cout<<"aajao";
+
+     return 0;
+ }
