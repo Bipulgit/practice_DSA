5e406d5e2d006107231b8a9a2b1bdbc8d0b25193
Bipul <bipul.kumar3384@gmail.com>
2023-08-24 18:49:18 +0530

    loop1.cpp

---
+++ loops 1.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"Find the first 10 natural number: "<<endl;
+	cout<<"---------------------------------"<<endl;
+	cout<<"The natural number are: "<<endl;
+	
+	for(i = 1; i <= 10; i++)
+	{
+		cout<< i <<" ";
+	}
+	cout<<endl;
+	
+}
