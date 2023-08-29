9e7bac17a427593523b24f3ab20ea2741f5734fa
Bipul <bipul.kumar3384@gmail.com>
2023-08-29 18:54:40 +0530

    pattern1.cpp

---
+++ pattern 1.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Print the pattern: "<<endl;
+	cout<<"------------------"<<endl;
+	cout<<"xxxxxxx"<<endl;
+	cout<<"xx    x       x       x"<<endl;
+	cout<<"xx            x       x"<<endl;
+	cout<<"xx            x       x"<<endl;
+	cout<<"xx          xxxxx   xxxxx"<<endl;
+	cout<<"xx            x       x "<<endl;
+	cout<<"xx            x       x"<<endl;
+	cout<<"xx    x       x       x"<<endl;
+	cout<<"xxxxxxx"<<endl;
+}
