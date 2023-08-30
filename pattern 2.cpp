fda6e1d39cca03226ea39f538ab56163c968d66c
Bipul <bipul.kumar3384@gmail.com>
2023-08-30 18:55:10 +0530

    pattern2.cpp

---
+++ pattern 2.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+                    //it's long way to make pattern
+int main()
+{
+	cout<<"Print the American flag:"<<endl;
+	cout<<"------------------------"<<endl;
+	cout<<"* * * * * * ============="<<endl;
+	cout<<" * * * * *  ============="<<endl;
+	cout<<"* * * * * * ============="<<endl;
+	cout<<" * * * * *  ============="<<endl;
+	cout<<"* * * * * * ============="<<endl;
+	cout<<"========================="<<endl;
+	cout<<"========================="<<endl;
+	cout<<"========================="<<endl;
+	cout<<"========================="<<endl;
+	cout<<"========================="<<endl;
+	cout<<"========================="<<endl;
+}
